215f568d49f9128ca3395f060cbd62592f10a5b8
Ryan Wang <wongyouth@gmail.com>
2024-02-28 16:20:10 +0800

    fix bug

--- lib/rucaptcha_no_cookies/view_helpers.rb
+++ lib/rucaptcha_no_cookies/view_helpers.rb
@@ -4,23 +4,25 @@ module RucaptchaNoCookies
 
     def rucaptcha_image_tag(opts = {})
       session_id = SecureRandom.hex(16)
-      rucaptcha_session_tag(session_id)
 
-      src = "#{ru_captcha.root_path}?#{SESSION_ID_KEY}=#{session_id}&t=#{Time.now.strftime('%s%L')}"
       opts[:class] = opts[:class] || "rucaptcha-image"
-      opts[:src] = src
+      opts[:src] = "#{ru_captcha.root_path}?#{SESSION_ID_KEY}=#{session_id}&t=#{Time.now.strftime('%s%L')}"
       opts[:onclick] = "this.src = '#{ru_captcha.root_path}?#{SESSION_ID_KEY}=#{session_id}&t=' + Date.now();"
-      tag(:img, opts)
+
+      tag.div(class: 'rucaptcha-image-group') do
+        rucaptcha_session_tag(session_id) + tag('img', opts)
+      end
     end
 
     private
 
     def rucaptcha_session_tag(value)
-      opts[:name] = SESSION_ID_KEY
-      opts[:id] = "rucaptcha-session-id"
-      opts[:type] = "hidden"
+      opts = {}
+      opts[:name]  = SESSION_ID_KEY
+      opts[:id]    = "rucaptcha-session-id"
+      opts[:type]  = "hidden"
       opts[:value] = value
-      tag(:input)
+      tag('input', opts)
     end
   end
 end
